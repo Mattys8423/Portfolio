987d0d4c439e3fd920facba1ca7bbd496c4b7f69
Kagutsuchi84 <115561782+Mattys8423@users.noreply.github.com>
2025-01-29 19:36:36 +0100

    Add files via upload

---
+++ JavaScript/menu.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('header nav');
+
+    // Ajoute ou enlève la classe "active" lorsque le bouton est cliqué
+    toggleButton.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
